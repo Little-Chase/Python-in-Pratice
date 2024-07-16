cfa0e5321ccaae34c3bfe36724ac8fc27aaaefcd
Little-Chase <chaseturner.edu@gmail.com>
2024-07-16 18:18:17 -0400

    Create category_1.py
    First Program

---
+++ category_1.py
@@ -0,0 +1,21 @@
+user_input = input("What is the category?")
+
+category = int(user_input)
+
+if category == 1:
+    print("mph 74-95")
+    
+elif category == 2:
+    print("mph 96-110")
+    
+elif category == 3:
+    print("mph 111-129")
+    
+elif category == 4:
+    print("mph 130-156")
+    
+elif category == 5:
+    print(" mph 157+")
+    
+else:
+    print("not a category")
